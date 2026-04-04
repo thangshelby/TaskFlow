7644ed99d07f0de325e71577714ff0722a888498
thangshelby <n.nducthangg@gmail.com>
2026-04-04 10:16:15 +0700

    [feature/ui-theme-redesign]  refactor: redesign the auth UI layout/theme

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -24,7 +24,7 @@ api.interceptors.response.use(
   async (error) => {
     if (error.response?.status === 401) {
       localStorage.removeItem("token");
-      // window.location.href = "/login";
+      // window.location.href = "/auth/login";
     }
     return Promise.reject(error);
   },
