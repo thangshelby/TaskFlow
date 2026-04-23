faa2c0c5052cc253d25200cceccce7568b6f3c1e
thangshelby <n.nducthangg@gmail.com>
2026-04-23 08:55:54 +0700

    refactor: enhance backlog UI with improved modal interactions, responsive layout adjustments, and issue editing state management.

--- src/app/pages/project/backLogPage/backLogPage.tsx
+++ src/app/pages/project/backLogPage/backLogPage.tsx
@@ -149,7 +149,7 @@ const BackLogPageContent: React.FC = () => {
                   maxSize={100}
                 >
                   <div className="h-full overflow-auto pr-4">
-                    <ul className="flex min-w-[550px] flex-col gap-2 overflow-x-auto">
+                    <ul className="flex min-w-[800px] flex-col gap-2">
                       {sprintIssues?.map((sprint: ISprintIssues) => (
                         <div key={sprint.id}>
                           <ScrumSprint
@@ -175,26 +175,28 @@ const BackLogPageContent: React.FC = () => {
                 </Panel>
 
                 {selectedIssueId && (
-                  <PanelResizeHandle
-                    style={{
-                      backgroundColor: "oklch(0.696 0.17 162.48)",
-                    }}
-                    className="backlog--panel-resize-handle relative w-[2px] cursor-col-resize bg-gray-300 pl-[2px] text-emerald-500 opacity-0 hover:opacity-100"
-                  />
+                  <>
+                    <PanelResizeHandle
+                      style={{
+                        backgroundColor: "oklch(0.696 0.17 162.48)",
+                      }}
+                      className="backlog--panel-resize-handle relative w-[2px] cursor-col-resize bg-gray-300 pl-[2px] text-emerald-500 opacity-0 hover:opacity-100"
+                    />
+                    <Panel
+                      id="issue-side-bar-panel"
+                      order={2}
+                      defaultSize={40}
+                      maxSize={50}
+                      minSize={30}
+                    >
+                      <div className="h-full overflow-y-auto pr-4">
+                        <Suspense fallback={<IssueDetailSkeleton />}>
+                          <IssueDetail selectedIssueId={selectedIssueId} />
+                        </Suspense>
+                      </div>
+                    </Panel>
+                  </>
                 )}
-                <Panel
-                  id="issue-side-bar-panel"
-                  order={2}
-                  defaultSize={selectedIssueId ? 40 : 0}
-                  maxSize={selectedIssueId ? 50 : 0}
-                  minSize={selectedIssueId ? 30 : 0}
-                >
-                  <div className="h-full overflow-y-auto pr-4">
-                    <Suspense fallback={<IssueDetailSkeleton />}>
-                      <IssueDetail selectedIssueId={selectedIssueId || ""} />
-                    </Suspense>
-                  </div>
-                </Panel>
               </PanelGroup>
             </SortableContext>
             <DragOverlay>
